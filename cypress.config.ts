b9d79fd6e0ce04d2bbec5022df4e79ffc09eb62b
ANS <36783010+answebdev@users.noreply.github.com>
2022-09-29 03:08:19 -0700

    add api key

---
+++ cypress.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  video: false,
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'https://isoundsmusic.netlify.app/',
+    excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*'],
+  },
+})
---
+++ cypress/e2e/tests/00_all.cy.js
@@ -0,0 +1,3 @@
+import './01_endpoints.cy';
+import './02_search_artists.cy';
+import './03_albums.cy';
