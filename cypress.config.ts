8f6f4f8d13bd79a3e22c52c1e1bdc687e0ee02d5
ANS <36783010+answebdev@users.noreply.github.com>
2024-01-21 17:41:22 -0800

    update config file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,15 @@
-/* eslint-disable no-unreachable */
 import { defineConfig } from 'cypress'
 
-module.exports = defineConfig({
+export default defineConfig({
   projectId: "2tsaw8",
   video: false,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config);
-      return config;
+      return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'https://isoundsmusic.netlify.app/',
     excludeSpecPattern: ['**/1-getting-started/*', '**/2-advanced-examples/*'],
   },
-});
+})
